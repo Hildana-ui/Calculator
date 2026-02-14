49b0957a0e70f1be9a9fa362f91146df8d755a91
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-02-14 13:57:13 +0300

    added html design and basic functions

---
+++ calc.js
@@ -0,0 +1,33 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const substract = (a, b) => {
+    return a - b;
+};
+
+const multiply = (a, b) => {
+    return a * b;
+};
+
+const divide = (a, b) => {
+    return a / b;
+};
+
+let a;
+let b;
+let operator;
+
+function operate(a, operator, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    } else {
+        return "Invalid Syntax";
+    }
+};
