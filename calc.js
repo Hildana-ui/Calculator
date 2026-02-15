41a61260d046c65bb371b4e3c473d87c7d58f8cf
Hildana-ui <hildana.amare@bitscollege.edu.et>
2026-02-15 23:27:27 +0300

    finished calculator logic

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@ const add = (a, b) => {
     return a + b;
 };
 
-const substract = (a, b) => {
+const subtract = (a, b) => {
     return a - b;
 };
 
@@ -14,9 +14,102 @@ const divide = (a, b) => {
     return a / b;
 };
 
-let a;
-let b;
-let operator;
+
+function clear() {
+    a = "";
+    b= "";
+    operator = null;
+
+    display.textContent = 0;
+}
+
+
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
+    clear()
+});
+
+
+let a = "";
+let b = "";
+let operator = null;
+let resetDisplay = false;
+
+const display = document.getElementById("display");
+
+function handleNum(num) {
+    if (resetDisplay) {
+        a = "";
+        resetDisplay = false;
+    }
+
+    if (operator === null) {
+        a += num;
+        display.textContent = a;
+    } else {
+        b += num;
+        display.textContent = b;
+    }
+}
+
+const numBtns = document.querySelectorAll(".num");
+
+numBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        handleNum(btn.textContent);
+    });
+});
+
+function handleOperator(op) {
+    if (a === "") return;
+
+    if (b != "") {
+        const result = operate(
+            Number(a),
+            operator,
+            Number(b)
+        );
+
+        a = result.toString();
+        display.textContent = result;
+        b = "";
+    }
+
+    operator = op;
+} 
+
+let operators = document.querySelectorAll(".op");
+
+operators.forEach(opBtn => {
+    opBtn.addEventListener("click", () => {
+        handleOperator(opBtn.textContent);
+    })
+})
+
+
+function handleEquals() {
+    if (a !== "" && b !== "" && operator !== null) {
+        const result = operate(
+            Number(a),
+            operator,
+            Number(b)
+        );
+
+        display.textContent = result;
+
+        a = result.toString();
+        b = "";
+        operator = null;
+        resetDisplay = true;
+    }
+};
+
+const equalsBtn = document.querySelector(".equals");
+
+equalsBtn.addEventListener("click", handleEquals);
+
+
+
 
 function operate(a, operator, b) {
     if (operator === "+") {
@@ -26,8 +119,8 @@ function operate(a, operator, b) {
     } else if (operator === "*") {
         return multiply(a, b);
     } else if (operator === "/") {
-        return divide(a, b);
+        return b === 0 ? "Error" : divide(a, b);
     } else {
-        return "Invalid Syntax";
+        return "ERROR";
     }
 };
